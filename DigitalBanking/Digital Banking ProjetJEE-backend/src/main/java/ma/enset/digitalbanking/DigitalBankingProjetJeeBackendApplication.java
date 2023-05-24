84264f3660cf10d982fc00d56b8522f105d5fdea
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-05-24 23:39:45 +0100

    init backend

---
+++ DigitalBanking/Digital Banking ProjetJEE-backend/src/main/java/ma/enset/digitalbanking/DigitalBankingProjetJeeBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.digitalbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingProjetJeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingProjetJeeBackendApplication.class, args);
+    }
+
+}
